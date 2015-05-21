dcfe20b0dad9c737f77d408a0150f328be57b883
lli5ba <lli5ba@virginia.edu>
2015-05-21 16:59:21 -0400

    added savings bar and threshold functionality

--- nessie/Nessie2.7.py
+++ nessie/Nessie2.7.py
@@ -44,14 +44,16 @@ if __name__ == "__main__":
     start_budget = 600.0
     start_balance = get_balance(account_id)
     budget_left = 600.0
+    threshold = 20
     ser = serial.Serial("/dev/tty.usbmodemfd121", 9600)
     #customers = get_customers()
     #print(customers[0].get("first_name"))
-    
+    #saving_goal = 800
+    #current_savings = 0
     while(budget_left > 0):
         curr_balance = get_balance(account_id)
-        if(curr_balance < 0):
-            print("balance below 0")
+        if(curr_balance < threshold):
+            print("balance below threshold")
         balance_diff = balance_change(account_id)
         if (balance_diff < 0):
             print( "blink red")
@@ -67,6 +69,14 @@ if __name__ == "__main__":
             #something for savings
             print("positive transaction: blink blue")
             ser.write(1.0)
+            """
+            current_savings += balance_diff
+             print(current_savings/savings_goal)
+            ser.write(str(current_savings/savings_goal))
+            """
+
+
+
 
 
     ser.close()
